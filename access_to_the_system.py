8a66e0439ebb088e1918cf4552ae3c38cda41ae7
Andrey Kovalenko <andreykovalenko94@gmail.com>
2025-12-12 22:07:47 +1000

    completed task access to the system

---
+++ access_to_the_system.py
@@ -0,0 +1,17 @@
+user_role = input('Введите вашу роль: ')
+user_experience = int(input('Введите ваш стаж: '))
+user_has_pass = input('Есть ли у вас пропуск? ')
+
+
+if user_has_pass == 'no':
+    print('Доступ запрещён: нет пропуска')
+elif user_role == 'intern':
+    print('Доступ запрещён: стажёр не имеет доступа')
+elif user_role == 'employee' and user_experience < 1:
+    print('Доступ запрещён: сотруднику требуется минимум 1 год опыта')
+elif user_role == 'manager' and user_experience < 2:
+    print('Доступ запрещён: менеджеру требуется стаж от 2 лет')
+elif user_role == 'admin':
+    print('Доступ разрешён: администратор')
+else:
+    print('Доступ разрешен')
